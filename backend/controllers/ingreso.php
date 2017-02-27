0c9441703e62639c08367cc1a5e1c037feb909bb
Manuel Beltràn <manubega@yahoo.com>
2017-02-26 21:45:49 -0600

    validación del usuario en la base de datos

--- backend/controllers/ingreso.php
+++ backend/controllers/ingreso.php
@@ -1,21 +1,88 @@
-<?php 
+<?php
 
 class Ingreso{
+
+	//METODO DE INGRESO A LA APLICACIÒN
 	public function ingresoController(){
 
-		$datosController = array( 'usuario' => $_POST['usuarioIngreso'],
-									'password' => $_POST['passwordIngreso']);
+		//COMPRUEBA SI VIENE INFORMACIÒN DEL USUARIO POR POST
+		if(isset($_POST["usuarioIngreso"])){
 
+			//VALIDACI'ON DE INGRESO DE CARACTERES ESPECIALES DEL USUARIO Y DEL PASSWORD
+			if(preg_match('/^[a-zA-Z0-9]+$/', $_POST["usuarioIngreso"])&&
+			   preg_match('/^[a-zA-Z0-9]+$/', $_POST["passwordIngreso"])){
 
-		$respuesta = IngresoModels::ingresoModel($datosController);
-		if($respuesta['usuario'] == $_POST['usuarioIngreso'] &&
-		$respuesta['password'] == $_POST['passwordIngreso']){
-			#header('Location:index.php?action=inicio');
-		}
-		else{
-			echo "<div class ='alert alert-danger'>ERROR AL INGRESAR</div>";
-		}
-			return $datosController;
-	}
-}
+			   	#$encriptar = crypt($_POST["passwordIngreso"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
 
+				$datosController = array("usuario"=>$_POST["usuarioIngreso"],
+				                     "password"=>$_POST["passwordIngreso"]);
+			//SE MANDAN LOS DATOS AL MODELO QUE VIENEN POR POST DE USUARIO Y PASSWORD
+				$respuesta = IngresoModels::ingresoModel($datosController, "usuarios");
+
+					//COMPRUEBA SI EL USUARIO EXISTE EN LA BASE DE DATOS
+				if($respuesta['usuario'] == $_POST['usuarioIngreso']){
+					$intentos = $respuesta["intentos"];
+					$usuarioActual = $_POST["usuarioIngreso"];
+					$maximoIntentos = 3;
+
+				//#1VALIDAMOS QUE LA CANTIDAD DE INTENTOS AL INGRESAR A LA BASE DE DATOS NO SEAN MAYORES A 3 
+				if($intentos <= $maximoIntentos){
+
+					//#2VALIDAMOS EL USUARIO Y EL PASSWORD QUE SEAN CORRECTOS PARA INGRESAR A LA PAGINA INICIO
+					if($respuesta["usuario"] == $_POST["usuarioIngreso"] && $respuesta["password"] == $_POST["passwordIngreso"]){
+
+						$intentos = 0;
+
+						$datosController = array("usuarioActual"=>$usuarioActual, "actualizarIntentos"=>$intentos);
+
+						$respuestaActualizarIntentos = IngresoModels::intentosModel($datosController, "usuarios");
+
+						session_start();
+
+						$_SESSION["validar"] = true;
+						$_SESSION["usuario"] = $respuesta["usuario"];
+
+						header("location:index.php?action=inicio");
+
+					}//#2 FIN
+
+					//SI EL USUARIO NO EXISTE EN LA BASE DE DATOS INTENTOS AUMENTA 1
+					else{
+
+						++$intentos;
+
+						$datosController = array("usuarioActual"=>$usuarioActual, "actualizarIntentos"=>$intentos);
+
+						$respuestaActualizarIntentos = IngresoModels::intentosModel($datosController, "usuarios");
+
+						echo '<div class="alert alert-danger">Error al ingresar</div>';
+
+					}
+				
+
+			
+
+				}#1 FIN
+					else{
+
+						$intentos = 0;
+
+						$datosController = array("usuarioActual"=>$usuarioActual, "actualizarIntentos"=>$intentos);
+
+						$respuestaActualizarIntentos = IngresoModels::intentosModel($datosController, "usuarios");
+
+						echo '<div class="alert alert-danger">varios intentos fallidos, demuestre que no es un robot</div>';
+
+				}
+					}//FIN SI EL USUARIO EXISTE EN LA BASE DE DATOS
+				else
+				{
+					echo '<div class="alert alert-danger">el Usuario no existe en la base de datos</div>';
+				}
+
+				//CUANDO LOS INTENTOS SEAN MAY
+				
+			}//FIN DE VALIDACION DE CARACTERES ESPECIALES
+		}//FIN VALIDACION DE INFORMACIÒN POR POS
+	}//FIN METODO
+}//FIN CLASE
--- backend/models/ingreso.php
+++ backend/models/ingreso.php
@@ -1,24 +1,41 @@
-<?php 
+<?php
 
-require_once("conexion.php");
+require_once "conexion.php";
 
 class IngresoModels{
 
-	public function ingresoModel($datosModel){
+	public function ingresoModel($datosModel, $tabla){
 
-		$stmt = Conexion::conectar() -> prepare("SELECT usuario,password FROM 
-			usuarios WHERE usuario = :usuario AND password = :password ");
-		$stmt -> bindParam(":usuario",$datosModel['usuario']);
-		$stmt -> bindParam(":password",$datosModel['password']);
+		$stmt = Conexion::conectar()->prepare("SELECT usuario, password, intentos FROM $tabla WHERE usuario = :usuario");
+
+		$stmt -> bindParam(":usuario", $datosModel["usuario"], PDO::PARAM_STR);
+		
 		$stmt -> execute();
 
 		return $stmt -> fetch();
 
 		$stmt -> close();
 
+	}
+
+	public function intentosModel($datosModel, $tabla){
+
+		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET intentos = :intentos WHERE usuario = :usuario");
+
+		$stmt -> bindParam(":intentos", $datosModel["actualizarIntentos"], PDO::PARAM_INT);
+		$stmt -> bindParam(":usuario", $datosModel["usuarioActual"], PDO::PARAM_STR);
+
+		if($stmt -> execute()){
+
+			return "ok";
+
+		}
+
+		else{
+
+			return "error";
+		}
 
 	}
+
 }
-
-
-
